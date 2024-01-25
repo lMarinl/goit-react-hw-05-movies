ee621ab58b570b5288f059ded58146cb9b525c05
lMarinl <marutunan67@gmail.com>
2024-01-25 23:21:08 +0200

    +

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,8 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 70,
+        color: 'green',
       }}
     >
       React homework template
